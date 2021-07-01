5e6478c5917238e0eb0f6e8a0e33d8bd9c34b28f
Lia Maccapani <73106505+liamaccapani@users.noreply.github.com>
2021-07-01 16:53:59 +0200

    Last commit
    (for now)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
   displayBoard()  
 }
 
-let displayBoard = function(){
+let displayBoard = function(){ //creates chart
     let cellsContainerNode = document.getElementById("board-container")
     for (let cellNumber = 1; cellNumber <= 76; cellNumber++) {
         let newCellNode = document.createElement("div")
@@ -12,6 +12,20 @@ let displayBoard = function(){
     }
 }
 
-const randomizeNumber = function(event){
-    
-}
+const randomizeNumber = function(){ //randomize number
+    let randomNumber = Math.floor(Math.random() * 76) + 1
+    return randomNumber
+} 
+
+// let selectCell = function() { //add .selected to div selected
+//     let cellsContainerNode = document.getElementById("board-container")
+//     let selectedCell = newCellNode.classList.add("selected")
+
+//     if (selectedCell === randomNumber) {
+//         selectedCell.style.backgroundcolor = "red"
+//     }
+// }
+
+
+
+//if selectedCell === randomNumber style.backgroundcolor = "darkgrey"
